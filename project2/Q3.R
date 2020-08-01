71840035547546f2169e9261b7bcb78b343652c2
munasirkm <48051099+munasirkm@users.noreply.github.com>
2020-08-01 23:29:35 +0530

    Create Q3.R

---
+++ project2/Q3.R
@@ -0,0 +1,18 @@
+library(data.table)
+library(ggplot2)
+setwd("F:/project/coursera/exploratory_analysis/project2")
+NEI <- as.data.table(readRDS("summarySCC_PM25.rds"))
+SCC <- as.data.table(readRDS("Source_Classification_Code.rds"))
+
+
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("Q3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type) + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
