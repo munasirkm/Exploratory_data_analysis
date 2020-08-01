046e7b58dd84ee7823e532a0cc55ade7805268ac
munasirkm <48051099+munasirkm@users.noreply.github.com>
2020-08-01 23:32:49 +0530

    Update Q3.R

--- project2/Q3.R
+++ project2/Q3.R
@@ -11,7 +11,7 @@ png("Q3.png")
 
 ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
   geom_bar(stat="identity") +
-  facet_grid(.~type) + 
+  facet_grid(.~type,scales = "free",space="free") + 
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
   labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
 
